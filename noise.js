b050f52a4ee5d9e5770587dfac1b431f15d46e3b
Kimberley Zell <kimberley.zell@gmail.com>
2017-11-21 09:38:40 -0800

    completed

--- noise.js
+++ noise.js
@@ -1,3 +1,19 @@
 $(document).ready( function() {
-  // your code here
+  // $('.instrument').click( function() {
+    // let audio = document.getElementById(`${$(this).html()}Audio`);
+
+  $('.instrument').on('click', 'button', function(event) {
+    let audio = document.getElementById(`${$(this).html()}Audio`);
+    audio.play();
+  });
+
+  $('body').keydown((event) => {
+    let keyInput = event.key;
+
+    let audio =  document.getElementById(`${keyInput}Audio`);
+
+    audio.play();
+  });
+
+
 });
